78af13f9da12adc7e0954ca60d9f4e9f4837454e
KHWCoding <rhscndcowlq@naver.com>
2023-05-07 22:00:53 +0900

    second commit

--- Spawner.cs
+++ Spawner.cs
@@ -9,6 +9,7 @@ public class Spawner : MonoBehaviour
 
     int level;
     float timer;
+    bool flag = false;
 
     void Awake()
     {
@@ -18,20 +19,36 @@ public class Spawner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 7f), spawnData.Length - 1);
+        level = Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 5f), 2);
+        // 오버라이딩 방식으로 몬스터에 따라 spawnTime을 지정해놓았으나 이는 시간에 따른 spawnTime을 다루는 데 이용
 
         if (timer > spawnData[level].spawnTime)
-        {       
+        {
             timer = 0;
             Spawn();
         }
+
+
+
+        if (!flag && GameManager.instance.gameTime>= GameManager.instance.maxGameTime) // 시간 조건만 따짐(포인트 관련 추가 예정)
+        {
+            boss_Spawn();
+        }
     }
 
-    void Spawn() //스폰관련 코드(현재는 몬스터 랜덤만 이후 포인트랑 시간을 통한 보스몹 소환 만들 예정)
+    void Spawn() //스폰관련 코드(일반 몬스터)
     {
         GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
         enemy.GetComponent<Enemy>().Init(spawnData[Random.Range(0,level+1)]);
+    }
+
+    void boss_Spawn() //보스 스폰관련(1마리만 소환)
+    {
+        flag = true;
+        GameObject enemy = GameManager.instance.pool.Get(0);
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[3]);
     }
 }
 
